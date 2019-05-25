dfba82938b7c85a125cb36e76f73fc29c571107e
mrctrl69 <aadjuust@gmail.com>
2019-05-25 18:30:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cashier/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.cashier;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView combos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        combos = findViewById(R.id.comboView1);
+        combos.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                openBeveragesMenu();
+            }
+        });
+    }
+
+    public void openCombosMenu() {
+        Intent intent =  new Intent(this, CombosActivity.class);
+        startActivity(intent);
+    }
+
+    public void openBeveragesMenu() {
+        Intent intent =  new Intent(this, BeveragesActivity.class);
+        startActivity(intent);
+    }
+
+    public void showIntentCombos(View view) {
+        displayToast(getString(R.string.intentCombos));
+    }
+
+    public void showIntentFoods(View view) {
+        displayToast(getString(R.string.intentFoods));
+    }
+
+    public void showIntentBeverages(View view) {
+        displayToast(getString(R.string.intentBeverages));
+    }
+
+    public void showIntentDesert(View view) {
+        displayToast(getString(R.string.intentDeserts));
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
